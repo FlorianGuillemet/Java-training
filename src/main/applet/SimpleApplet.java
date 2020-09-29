a5cd55e4c5b83a9c409ae32b531fb192fe935c1a
Florian <florian.guillemet@mythalesgroup.io>
2020-09-29 17:59:17 +0200

    Applet

---
+++ src/main/applet/SimpleApplet.java
@@ -0,0 +1,30 @@
+package main.applet;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.applet.*;
+import java.awt.*;
+
+public class SimpleApplet extends Applet {
+
+    public void init() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public void destroy() {
+
+    }
+
+    public void paint(Graphics grph) {
+        grph.drawString("Je suis Florian", 100, 67);
+    }
+
+}
